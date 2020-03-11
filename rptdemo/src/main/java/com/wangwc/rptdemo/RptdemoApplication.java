12dadbd361009f6f820458809e401273f4f0a35b
wangwence <820653253@qq.com>
2020-03-12 01:36:44 +0800

    添加rptdemo模

---
+++ rptdemo/src/main/java/com/wangwc/rptdemo/RptdemoApplication.java
@@ -0,0 +1,13 @@
+package com.wangwc.rptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RptdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RptdemoApplication.class, args);
+    }
+
+}
---
+++ rptdemo/src/test/java/com/wangwc/rptdemo/RptdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangwc.rptdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RptdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
